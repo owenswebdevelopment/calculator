0d93a656e523aef7dad6fe5d29105370569c8a02
Owen <139211753+owenswebdevelopment@users.noreply.github.com>
2023-09-21 00:58:24 +0900

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -21,7 +21,7 @@ for (let key of keys){
     }
     else if ( value == "="){
       let result = eval(input);
-     display_Output.innerHTML = result;
+     display_Output.innerHTML = cleanOutput(result);
     }
     else if (value == "brackets"){
       if (
@@ -47,7 +47,7 @@ for (let key of keys){
     }
 else {
   input += value;
-  display_Input.innerHTML = input;
+  display_Input.innerHTML = cleanInput(input);
 }
       
   })
@@ -60,32 +60,58 @@ function cleanInput(input){
   let input_array_length = input_array.length;
 
   for (let i = 0; i < input_array_length; i++){
-    console.log(input_array[i]); 
     if (input_array[i] == "*"){
-    input_array[i] = ` <span class="operator">x</span> `;
-  }
-  else if(input_array[i] == "/"){
-    input_array[i] = ` <span class="operator">÷</span>`;
-  }
-  else if(input_array[i] == "+"){
-    input_array[i] = ` <span class="operator">+</span>`;
+      input_array[i] = ` <span class="operator">x</span> `;
+    }
+    else if(input_array[i] == "/"){
+      input_array[i] = ` <span class="operator">÷</span>`;
+    }
+    else if(input_array[i] == "+"){
+      input_array[i] = ` <span class="operator">+</span>`;
+    }
+    else if(input_array[i] == "-"){
+      input_array[i] = ` <span class="operator">-</span>`;
+    }
+    else if(input_array[i] == "="){
+      input_array[i] = ` <span class="operator">=</span>`;
+    }
+    else if (input_array[i] == "%") {
+      input_array[i] = `<span class="percent">%</span>`;
+    } 
+    else if (input_array[i] == "(") {
+      input_array[i] = `<span class="brackets">(</span>`;
+    }
+    else if (input_array[i] == ")") {
+      input_array[i] = `<span class="brackets">)</span>`;
+    } 
   }
   
-  else if(input_array[i] == "-"){
-    input_array[i] = ` <span class="operator">-</span>`;
-  }
-
-  else if(input_array[i] == "="){
-    input_array[i] = ` <span class="operator">=</span>`;
-  }
-  else if (input_array[i] == "%") {
-    input_array[i] = `<span class="percent">%</span>`;
-} 
-}
-return input_array.join = ("");
+  return input_array.join("");
 }
 
+function cleanOutput(output){
+let output_string = output.toString();
+let decimal = output_string.split(".")[1];
+output_string = output_string.split(".")[0];
+
+let output_array = output_string.split("");
+
+if (output_array.length > 3){
+  for (let i = output_array.length - 3; i > 0; i -= 3) {
+    output_array.splice(i, 0, ",");
+
+  }
+}
+if (decimal){
+  output_array.push(".");
+  output_array.push(decimal);
+}
+return output_array.join("");
+}
   
+
+
+
 
 
 
